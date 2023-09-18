be20b215a5c0a8c0552f7af247cbb7682b9aebc0
“hiran_xdoto” <hiran@xdoto.io>
2023-09-18 15:33:30 +0530

    Add api calls functions in EmployeeService

---
+++ src/main/java/io/xdoto/springbootsamplecrud/service/EmployeeService.java
@@ -0,0 +1,28 @@
+package io.xdoto.springbootsamplecrud.service;
+
+import io.xdoto.springbootsamplecrud.dto.Employee;
+import io.xdoto.springbootsamplecrud.dto.ResponseStructure;
+
+import java.util.List;
+
+public class EmployeeService {
+    public ResponseStructure<Employee> saveEmployee(Employee employee) {
+        return null;
+    }
+
+    public ResponseStructure<Employee> getStudentById(int id) {
+        return null;
+    }
+
+    public ResponseStructure<List<Employee>> getAllEmployee() {
+        return null;
+    }
+
+    public ResponseStructure<Employee> updateEmployee(Employee employee, Integer id) {
+        return null;
+    }
+
+    public ResponseStructure<String> deleteEmployee(int id) {
+        return null;
+    }
+}
