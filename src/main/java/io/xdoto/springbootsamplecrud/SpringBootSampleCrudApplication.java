5d83724580eaea9aa95f49d29ce558476947bb0e
“hiran_xdoto” <hiran@xdoto.io>
2023-09-18 14:20:51 +0530

    Initial commit

---
+++ src/main/java/io/xdoto/springbootsamplecrud/SpringBootSampleCrudApplication.java
@@ -0,0 +1,13 @@
+package io.xdoto.springbootsamplecrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSampleCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSampleCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/xdoto/springbootsamplecrud/SpringBootSampleCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package io.xdoto.springbootsamplecrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSampleCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
